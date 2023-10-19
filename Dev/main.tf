cd87fa3de52a8292b36d40626142adae37c97c5a
prasanna kumar balla <ballaprasanna1769@gmail.com>
2023-10-19 06:35:21 +0000

    Updated main.tf

--- Dev/main.tf
+++ Dev/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
 module "dev" {
   source               = "./.module"
   prefix = "dev"
