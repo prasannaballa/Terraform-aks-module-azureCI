500e783d43024e8b686e7f2fa45b6bdba75d9a7c
prasanna kumar balla <ballaprasanna1769@gmail.com>
2023-10-19 11:12:32 +0000

    Added dev.tf

---
+++ dev.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+module "module_dev" {
+  source = "./modules"
+  prefix = "dev"
+  resource_group_name = "aks_tf_rg"
+  location            = "CentralUS"
+  cluster_name        = "devakspras1769"
+  kubernetes_version  = "1.26.3"
+  system_node_count   = 2
+  acr_name            = "myacrdev1234"            
+}
