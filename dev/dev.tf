47c1984fc873a926b2562d316f3345394c8aac16
prasanna kumar balla <ballaprasanna1769@gmail.com>
2023-10-20 04:51:16 +0000

    Added dev.tf

---
+++ dev/dev.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "Terraform"
+    storage_account_name = "mytfstorage12345"
+    container_name = "aksmodule"
+    key = "terraform.storage"
+    access_key = "k+m0krF3hiuX9TSshFHlrttm/2r9Ne58J8U6aPNHVlaAEp/48YuYGvhI19Y2ixdU0/AYZsYhih70+AStinPztQ=="
+  }
+}
+module "dev" {
+  source      = "./module"
+  prefix = "dev"
+  resource_group_name = "aksdev"
+  location            = "CentralUS"
+  cluster_name        = "devakspras0769"
+  kubernetes_version  = "1.26.3"
+  system_node_count   = 2
+  acr_name            = "myacrdev1234"            
+}
