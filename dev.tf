6a53f34d6a48e209324fdce389b114c0faca3c71
prasanna kumar balla <ballaprasanna1769@gmail.com>
2023-10-20 04:22:50 +0000

    Updated dev.tf

--- dev.tf
+++ dev.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "Terraform"
+    storage_account_name = "mytfstorage12345"
+    container_name = "terraformsf"
+    key = "terraform.vmstorage"
+    access_key = "k+m0krF3hiuX9TSshFHlrttm/2r9Ne58J8U6aPNHVlaAEp/48YuYGvhI19Y2ixdU0/AYZsYhih70+AStinPztQ=="
+  }
+}
 module "dev" {
   source      = "./modules"
   resource_group_name = "aks_tf_rg"
