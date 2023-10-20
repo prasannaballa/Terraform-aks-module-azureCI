06b6f3372621a3aa9d1b531b67cfd7e514c9c0bb
prasanna kumar balla <ballaprasanna1769@gmail.com>
2023-10-20 05:27:18 +0000

    Updated qat.tf

--- qat.tf
+++ qat.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "Terraform"
-    storage_account_name = "mytfstorage12345"
-    container_name = "terraformqat"
-    key = "terraform.storage"
-    access_key = "k+m0krF3hiuX9TSshFHlrttm/2r9Ne58J8U6aPNHVlaAEp/48YuYGvhI19Y2ixdU0/AYZsYhih70+AStinPztQ=="
-  }
-}
 module "qat" {
   source      = "./module"
   prefix = "qat"
